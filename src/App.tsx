815355308a35179cbd1ebf9ca50a80294577b765
MamatovY <mamatovyiman28@gmail.com>
2024-03-19 16:41:04 +0600

    add userCard & UserStat

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { TheHeader } from "components/TheHeader";
 import { Container } from "./components/Container";
 import { Search } from "components/Search";
+import { UserCard } from "components/UserCard";
+import { defaultUser } from "mock";
 
 function App() {
   return (
     <Container>
       <TheHeader />
       <Search hasError onSubmit={() => { }} />
+      <UserCard {...defaultUser} />
     </Container>
   );
 }
---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "MamatovY",
+    "id": 77632341,
+    "avatar": "https://avatars.githubusercontent.com/u/77632341?v=4",
+    "name": "Маматов Ыйманбек",
+    "company": '',
+    "blog": "https://my-portfolio-mamatov.vercel.app/",
+    "location": '',
+    "bio": "Я Frontent middle-разработчик, который хочет создавать красивые, интуитивно понятные и эффективные веб-приложений.",
+    "twitter": '',
+    "repos": 37,
+    "followers": 1,
+    "following": 1,
+    "created": "2021-01-18T16:12:53Z",
+}
